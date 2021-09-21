610b2d915bafa99044a739380872459097bff772
Leo Spratt <enchant97@users.noreply.github.com>
2021-09-22 00:09:00 +0100

    add about page

--- SimpleBackup.InterfaceGtk/Program.cs
+++ SimpleBackup.InterfaceGtk/Program.cs
@@ -18,42 +18,8 @@ namespace SimpleBackup.InterfaceGtk
             MainWin = new Views.MainWindow();
             App.AddWindow(MainWin);
 
-
-            GLib.Menu menu = new();
-
-            GLib.MenuItem fileItem = new("File", "app.file");
-            GLib.Menu fileMenu = new();
-            fileMenu.AppendItem(new GLib.MenuItem("Quit", "app.quit"));
-            fileItem.Submenu = fileMenu;
-
-            GLib.MenuItem helpItem = new("Help", "app.help");
-            GLib.Menu helpMenu = new();
-            helpMenu.AppendItem(new GLib.MenuItem("About", "app.about"));
-            helpItem.Submenu = helpMenu;
-
-            menu.AppendItem(fileItem);
-            menu.AppendItem(helpItem);
-
-            App.AppMenu = menu;
-
-            GLib.SimpleAction quitAction = new("quit", null);
-            quitAction.Activated += QuitEvent;
-            App.AddAction(quitAction);
-
-            GLib.SimpleAction aboutAction = new("about", null);
-            aboutAction.Activated += AboutEvent;
-            App.AddAction(aboutAction);
-
             MainWin.ShowAll();
             Application.Run();
-        }
-        private static void QuitEvent(object sender, EventArgs e)
-        {
-            Application.Quit();
-        }
-        private static void AboutEvent(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- SimpleBackup.InterfaceGtk/Views/MainWindow.cs
+++ SimpleBackup.InterfaceGtk/Views/MainWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using Gtk;
 using SimpleBackup.Core;
 
@@ -9,16 +10,52 @@ namespace SimpleBackup.InterfaceGtk.Views
         {
             SetDefaultSize(400, 200);
             SetPosition(WindowPosition.Center);
-            DeleteEvent += OnDelete;
+            DeleteEvent += OnQuit;
 
-            Label tempMessage = new(
-                "SimpleBackup GUI is not currently implemented");
+            VBox mainBox = new(false, 2);
 
-            Add(tempMessage);
+            MenuBar menuBar = new();
+            Menu file = new();
+            MenuItem mFile = new("File") { Submenu = file };
+            MenuItem mQuit = new("Quit");
+            mQuit.Activated += OnQuit;
+            Menu help = new();
+            MenuItem mHelp = new("Help") { Submenu = help };
+            MenuItem mAbout = new("About");
+            mAbout.Activated += OnAbout;
+
+            file.Append(mQuit);
+            help.Append(mAbout);
+            menuBar.Append(mFile);
+            menuBar.Append(mHelp);
+            menuBar.Append(file);
+
+            Label title = new(Constants.AppName + " - GUI MODE");
+            Label currConfigName = new();
+
+            mainBox.PackStart(menuBar, false, false, 0);
+            mainBox.PackStart(title, false, false, 14);
+            mainBox.PackStart(currConfigName, false, false, 0);
+            Add(mainBox);
         }
-        private void OnDelete(object obj, DeleteEventArgs args)
+        private void OnQuit(object obj, EventArgs args)
         {
             Application.Quit();
+        }
+        private void OnAbout(object obj, EventArgs args)
+        {
+            AboutDialog aboutDialog = new();
+            aboutDialog.ProgramName = Constants.AppName;
+            aboutDialog.Copyright = "GPL-3.0 (c) Leo Spratt";
+            aboutDialog.Comments = "Welcome to Simple Backup, here is some help to get you started:\n\n" +
+                                   "- You can do full-backups\n" +
+                                   "- You can include and exclude paths to backup\n" +
+                                   "- You can set versions of backups to keep\n" +
+                                   "- You can keep different configurations for backups\n" +
+                                   "- Once everything is configured you can backup with one command\n" +
+                                   "- All configs are stored in a xml file\n";
+            aboutDialog.Run();
+            aboutDialog.Destroy();
         }
     }
 }
