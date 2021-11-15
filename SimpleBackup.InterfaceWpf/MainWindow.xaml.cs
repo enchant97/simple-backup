226735d17fe8fb01230f75cfa56a84da6aacf586
Leo Spratt <enchant97@users.noreply.github.com>
2021-11-15 16:46:34 +0000

    add blank wpf project

---
+++ SimpleBackup.InterfaceWpf/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace SimpleBackup.InterfaceWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuExitBnt_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuGetStartedBnt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuAboutBnt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
