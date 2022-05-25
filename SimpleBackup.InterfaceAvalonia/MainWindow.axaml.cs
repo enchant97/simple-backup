c3a4648ae217d642f244e1aa9c1e8450ad68362b
Leo Spratt <enchant97@users.noreply.github.com>
2022-05-25 11:28:55 +0100

    add export/import

--- SimpleBackup.InterfaceAvalonia/MainWindow.axaml.cs
+++ SimpleBackup.InterfaceAvalonia/MainWindow.axaml.cs
@@ -1,5 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using MessageBox.Avalonia;
+using MessageBox.Avalonia.Enums;
+using MessageBox.Avalonia.BaseWindows.Base;
 using SimpleBackup.Core.Configuration;
 
 namespace SimpleBackup.InterfaceAvalonia
@@ -22,6 +25,59 @@ namespace SimpleBackup.InterfaceAvalonia
 
                 QuickConfig.AppConfig.ShowHelp = false;
                 QuickConfig.Write();
+            }
+        }
+        private async void OnClickMenuImportConfig(object sender, RoutedEventArgs e)
+        {
+            IMsBoxWindow<ButtonResult> prompt = MessageBoxManager.GetMessageBoxStandardWindow(
+                "Confirm",
+                "Importing a new config will\noverwrite your current one.",
+                ButtonEnum.OkCancel
+            );
+            ButtonResult promptResult = await prompt.Show();
+            if (promptResult == ButtonResult.Ok)
+            {
+                OpenFileDialog dialog = new()
+                {
+                    AllowMultiple = false,
+                    Filters = {
+                        new() {
+                        Extensions = {".xml"},
+                        Name = "XML Files"
+                        }
+                    }
+                };
+                string[]? filepaths = await dialog.ShowAsync(this);
+                if (filepaths != null)
+                {
+                    string filepath = filepaths[0];
+                    MainStatus.Content = "Importing new config";
+                    QuickConfig.Read(filepath);
+                    QuickConfig.Write();
+                    // FIXME: refresh UI here
+                    MainStatus.Content = "Imported new config";
+                }
+            }
+        }
+        private async void OnClickMenuExportConfig(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new()
+            {
+                DefaultExtension = ".xml",
+                Filters =
+                {
+                    new() {
+                        Extensions = {".xml"},
+                        Name = "XML Files"
+                    }
+                }
+            };
+            string? filepath = await dialog.ShowAsync(this);
+            if (filepath != null)
+            {
+                MainStatus.Content = "Exporting config";
+                QuickConfig.Write(filepath);
+                MainStatus.Content = "Done Exporting config";
             }
         }
         private void OnClickMenuExit(object sender, RoutedEventArgs e)
