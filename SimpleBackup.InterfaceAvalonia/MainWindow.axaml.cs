585836383cb954ffd77248b4cc3d9673eac648f3
Leo Spratt <enchant97@users.noreply.github.com>
2022-05-26 14:27:23 +0100

    add backup start&stop

--- SimpleBackup.InterfaceAvalonia/MainWindow.axaml.cs
+++ SimpleBackup.InterfaceAvalonia/MainWindow.axaml.cs
@@ -6,19 +6,48 @@ using MessageBox.Avalonia.BaseWindows.Base;
 using SimpleBackup.Core.Configuration;
 using System;
 using SimpleBackup.Core.Configuration.Types;
+using System.Collections.ObjectModel;
+using System.Threading;
+using SimpleBackup.Core.Backup;
+using SimpleBackup.Core;
+using Avalonia.Threading;
+using SimpleBackup.Core.Paths;
 
 namespace SimpleBackup.InterfaceAvalonia
 {
     public partial class MainWindow : Window
     {
+        #region Fields
+        private Thread? backupThread;
+        private BackupHandler? backupHandler;
+        private readonly ObservableCollection<string> loggedErrors;
+        public int FoundCount { get; private set; }
+        public int CopiedCount { get; private set; }
+        #endregion
         public MainWindow()
         {
+            loggedErrors = new();
+
             Opened += (sender, evt) => OnWindowLoad();
             InitializeComponent();
+
+            LoggedErrorsLB.Items = loggedErrors;
             ShowBackupConfigs();
             ShowSelectedConfigOnUI();
         }
         #region UI Helpers
+        public static string ExceptionToString(BackupHandlerErrorEventArgs args)
+        {
+            string errorMsg = args.ErrorType switch
+            {
+                Constants.ErrorTypes.NO_PERMISSION => "No permission at",
+                Constants.ErrorTypes.NOT_COPYABLE_TYPE => "Not copyable type at",
+                Constants.ErrorTypes.NOT_FOUND => "Not found at",
+                _ => "Unhandled error at"
+
+            };
+            return string.Format("{0} '{1}'", errorMsg, args.FullPath);
+        }
         private void ShowBackupConfigs()
         {
             CurrConfigCB.Items = QuickConfig.AppConfig.BackupConfigs;
@@ -26,12 +55,120 @@ namespace SimpleBackup.InterfaceAvalonia
         }
         private void ShowSelectedConfigOnUI()
         {
-            if (CurrConfigCB.SelectedItem != null) { 
+            if (CurrConfigCB.SelectedItem != null)
+            {
                 BackupConfig backupConfig = (BackupConfig)CurrConfigCB.SelectedItem;
                 LastBackupLabel.Content = backupConfig.LastBackup;
                 DestinationLabel.Content = backupConfig.DestinationPath;
                 TypeLabel.Content = Enum.GetName(backupConfig.BackupType);
             }
+        }
+        private void BackupRunningUi(bool backupRunning)
+        {
+            CurrConfigCB.IsEnabled = !backupRunning;
+            MenuImportConfigBnt.IsEnabled = !backupRunning;
+            MenuPreferencesBnt.IsEnabled = !backupRunning;
+
+            if (backupRunning)
+                StartBackupBnt.Content = "Stop Backup";
+            else
+                StartBackupBnt.Content = "Start Backup";
+        }
+        #endregion
+        #region Backup Handlers
+        private void HandleBackupDiscovery()
+        {
+            FoundCount++;
+            MainStatus.Content = string.Format("Found {0}", FoundCount);
+        }
+        private void HandleBackupCopied()
+        {
+            CopiedCount++;
+            MainStatus.Content = string.Format("Copied {0}/{1}", CopiedCount, FoundCount);
+        }
+        private void HandleBackupException(BackupHandlerErrorEventArgs args)
+        {
+            loggedErrors.Add(ExceptionToString(args));
+        }
+        private void HandleBackupClean()
+        {
+            MainStatus.Content = "Removing old backups";
+        }
+        private void HandleBackupFinished()
+        {
+            QuickConfig.AppConfig.BackupConfigs[CurrConfigCB.SelectedIndex].LastBackup = DateTime.UtcNow;
+            QuickConfig.Write();
+
+            if (loggedErrors.Count > 0)
+                MainStatus.Content = "Backup Finished (With Errors)";
+            else
+                MainStatus.Content = "Backup Finished";
+            backupThread = null;
+            backupHandler = null;
+            BackupRunningUi(false);
+        }
+        #endregion
+        #region Backup Control
+        private async void StartBackup(BackupConfig backupConfig)
+        {
+            await Dispatcher.UIThread.InvokeAsync(
+                () =>
+                {
+                    BackupRunningUi(true);
+                    loggedErrors.Clear();
+                    FoundCount = 0;
+                    CopiedCount = 0;
+                    MainStatus.Content = "Backup Starting";
+                },
+                DispatcherPriority.Background
+            );
+
+            string backupDstPath = Generation.GenerateBackupName(
+                backupConfig.DestinationPath,
+                backupConfig.BackupType
+            );
+
+            backupHandler = new BackupHandler(
+                backupDstPath,
+                backupConfig.IncludedPaths.ToArray(),
+                backupConfig.ExcludedPaths.ToArray(),
+                QuickConfig.AppConfig.ExcludedFilenames,
+                backupConfig.BackupType,
+                false
+            );
+
+            // setup events
+            backupHandler.DiscoveryEvent += (object? sender, BackupHandlerEventArgs args) =>
+            {
+                Dispatcher.UIThread.Post(HandleBackupDiscovery);
+            };
+            backupHandler.CopyEvent += (object? sender, BackupHandlerEventArgs args) =>
+            {
+                Dispatcher.UIThread.Post(HandleBackupCopied);
+            };
+            backupHandler.ExceptionDiscoveringEvent += (object? sender, BackupHandlerErrorEventArgs args) =>
+            {
+                Dispatcher.UIThread.Post(() => HandleBackupException(args));
+            };
+            backupHandler.ExceptionCopyEvent += (object? sender, BackupHandlerErrorEventArgs args) =>
+            {
+                Dispatcher.UIThread.Post(() => HandleBackupException(args));
+            };
+            backupHandler.FinishedEvent += async (object? sender, EventArgs args) =>
+            {
+                if (backupConfig.VersionsToKeep > 0)
+                {
+                    await Dispatcher.UIThread.InvokeAsync(HandleBackupClean);
+                    int backupsRemoved = Cleaning.RemovePreviousBackups(
+                        backupConfig.VersionsToKeep,
+                        backupConfig.DestinationPath
+                    );
+                }
+                Dispatcher.UIThread.Post(HandleBackupFinished);
+            };
+
+            // start backup
+            backupHandler.Start();
         }
         #endregion
         #region Events
@@ -125,6 +262,32 @@ namespace SimpleBackup.InterfaceAvalonia
         private void OnSelectionCurrConfigCB(object sender, SelectionChangedEventArgs e)
         {
             ShowSelectedConfigOnUI();
+        }
+        private void OnClickStartStopBackup(object sender, RoutedEventArgs e)
+        {
+            if (backupThread != null)
+            {
+                if (backupHandler != null)
+                {
+                    loggedErrors.Add("Backup stopped by user...");
+                    backupHandler.Stop();
+                }
+                return;
+            }
+            BackupConfig backupConfig = QuickConfig.AppConfig.BackupConfigs[CurrConfigCB.SelectedIndex];
+            if (string.IsNullOrEmpty(backupConfig.DestinationPath))
+            {
+                IMsBoxWindow<ButtonResult> prompt = MessageBoxManager.GetMessageBoxStandardWindow(
+                    "Cannot Start Backup",
+                    "No backup destination path has been set",
+                    ButtonEnum.Ok,
+                    MessageBox.Avalonia.Enums.Icon.Warning
+                );
+                prompt.ShowDialog(this);
+                return;
+            }
+            backupThread = new(() => StartBackup(backupConfig));
+            backupThread.Start();
         }
         #endregion
     }
